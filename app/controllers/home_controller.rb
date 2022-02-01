9e165058505a31a5ccf3e8807a2ef27b8a38f2c6
Masaya Konishi <konishi@office-ore.net>
2022-02-01 17:17:48 +0900

    google ログイン

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  skip_before_action :check_logged_in
+
   def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get 'home/index'
+
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'log_out', to: 'sessions#destroy', as: 'log_out'
+
+  resources :sessions, only: %i[create destroy]
 end
